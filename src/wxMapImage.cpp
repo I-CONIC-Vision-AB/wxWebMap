3564311e64d07fcbc879c4fe525f32a7e1da4d7d
sardnas <74879871+sardnas@users.noreply.github.com>
2022-04-30 11:45:19 +0200

    moved repeated functions to wxMapObject

--- src/wxMapImage.cpp
+++ src/wxMapImage.cpp
@@ -2,43 +2,7 @@
 #include    <wx/tokenzr.h>
 #include    <wx/log.h>
 #include    <wx/intl.h>
-
-wxString wxMapObject::GetRemoveStringImage(wxString const& map)
-{
-    return wxString::Format("mapobject_remove(%d, %s); \n", cLeafletId, map);
-}
-
-bool wxMapObject::ParseResultImage(wxString const& result, EMapObjectType& type, int& id)
-{
-    wxStringTokenizer parse(result, ",");
-    int i = 0;
-    bool bOk = true;
-    while (parse.HasMoreTokens()) {
-        wxString token = parse.GetNextToken();
-        switch (i) {
-        case 0:
-            // Parse type:
-            if (token.IsSameAs("IMAGE")) {
-                type = EMapObjectType::IMAGE;
-            }
-            break;
-        case 1:
-
-        {
-            // Parse id
-            long lid;
-            if (!token.ToLong(&lid)) {
-                wxLogError(_("Could not parse result as id: %s"), token);
-                bOk = false;
-            }
-            id = (int)lid;
-        }
-        break;
-        }
-        ++i;
-    }
-    return bOk;
-}
+#include <wxMapObject.h>
 
 wxMapImage::wxMapImage(double upperLeftLat, double upperLeftlon, double lowerRightLat, double lowerRightLon, wxString imageUrl) :
     cLeftLat(upperLeftLat),
