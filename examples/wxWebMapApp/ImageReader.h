48895e40bf1b8a84269b622eaaeea8db3691a1cf
HWiman-ICONIC <78019492+HWiman-ICONIC@users.noreply.github.com>
2022-05-03 22:31:15 +0200

    Added documentation of Polygon|ImageReader

--- examples/wxWebMapApp/ImageReader.h
+++ examples/wxWebMapApp/ImageReader.h
@@ -3,6 +3,10 @@
 #include	<wxMapUtil.h>
 #include    <wx/textfile.h>
 
+/**
+ * @brief Reads text files with polygons and image file names.
+ * @todo Hardcoded thumbnail path + prefix are added for this esample application
+*/
 class ImageReader {
 public:
     ImageReader(wxString const& filename, std::vector<std::pair<wxMapPoint, wxMapPoint>>& imagePoints, std::vector<wxString>& imagePaths);
--- examples/wxWebMapApp/PolygonReader.h
+++ examples/wxWebMapApp/PolygonReader.h
@@ -3,9 +3,26 @@
 #include	<wxMapUtil.h>
 #include    <wx/textfile.h>
 
+/**
+ * @brief Reads text files with footprints.
+*/
 class PolygonReader {
 public:
+    /**
+     * @brief Constructor reads footprints from file.
+     * @param filename I-CONIC footprint fie (*.ifp)
+     * @param polygons Read polygons
+     * @param metaData Name of each polygon
+    */
     PolygonReader(wxString const& filename, std::vector<std::vector<wxMapPoint>>& polygons, std::vector<wxString>& metaData);
 protected:
+    /**
+     * @brief Helper reading a single polygon
+     * @param file File
+     * @param s String
+     * @param polygon Polygon
+     * @param meta Name of polygon
+     * @return True on success
+    */
     bool ReadPolygon(wxTextFile& file, wxString& s, std::vector<wxMapPoint >& polygon, wxString& meta);
 };
