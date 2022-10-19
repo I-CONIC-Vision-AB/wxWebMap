a29a5c987f9086194fd6a3290014c287f344f352
Andreas <andreas.vralstad@gmail.com>
2022-10-19 18:07:30 +0200

    Can now build debug and release on Windows

--- install-dependencies.bat
+++ install-dependencies.bat
@@ -2,9 +2,3 @@
 
 REM Make sure all submodules are initialized
 git submodule update --init --recursive
-
-REM Bootstrap vcpkg (ie. download the vcpkg executable)
-REM vcpkg\bootstrap-vcpkg.bat
-
-REM Install required packages
-REM vcpkg\vcpkg.exe install --triplet x64-windows boost-chrono
